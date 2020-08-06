bbbb985f94eabb81d9de7ff3f7d50e03da8ed5bf
Jason Lengstorf <jason@lengstorf.com>
2020-08-06 15:58:44 -0700

    feat: episode meta

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,11 +1,10 @@
 module.exports = {
   // keep it short! shown in the header
-  title: 'Stripe Subscriptions',
+  title: 'Contentful, GraphQL, & Paid Content',
 
   // these are all optional and add links to the footer
-  repo: 'stripe-samples/netlify-stripe-subscriptions',
-  episode:
-    'https://www.learnwithjason.dev/subscription-management-in-jamstack-apps',
-  tutorial:
-    'https://www.netlify.com/blog/2020/07/13/manage-subscriptions-and-protect-content-with-stripe?utm_source=demo-footer&utm_medium=stripe-subs-jl&utm_campaign=devex',
+  repo: 'learnwithjason/contentful-stripe-paywall',
+  episode: 'https://www.learnwithjason.dev/contentful-graphql-paid-content',
+  // tutorial:
+  //   'https://www.netlify.com/blog/2020/07/13/manage-subscriptions-and-protect-content-with-stripe?utm_source=demo-footer&utm_medium=stripe-subs-jl&utm_campaign=devex',
 };
